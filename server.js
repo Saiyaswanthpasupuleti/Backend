4c3472eb29e6a46de573a56053e5baf7bdadaad1
Sai Yaswanth <saiyaswanth@192.168.1.2>
2025-04-18 15:42:12 +0530

    new

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.get('/user', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('🚀 Server is running on http://localhost:3000');
+app.listen(3001, () => {
+  console.log('🚀 Server is running on http://localhost:3001');
 });
